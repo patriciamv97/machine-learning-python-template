ef0d55f89d1d838058d5adc3db2f5d15ebe954b1
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-04 20:36:24 +0000

    comienzo modelo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
 from utils import db_connect
 engine = db_connect()
 
 # your code here
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('/workspaces/machine-learning-python-template/data/raw/AB_NYC_2019.csv')
+data.to_sql('2019-ABNB', engine, if_exists='replace', index=False)
